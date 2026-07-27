ffc31e459198ab774bc9f6b40035a241ed73b842
Aryan Patel <aryanpatel2906.ap@gmail.com>
2026-07-27 17:39:21 +0530

    Create 2230A.cpp

---
+++ practice/800/2230A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n, a, b;
+        cin >> n >> a >> b;
+
+        if (b >= 3 * a) {
+            cout << n * a << '\n';
+        } else {
+            cout << (n / 3) * b + min((n % 3) * a, b) << '\n';
+        }
+    }
+
+    return 0;
+}
